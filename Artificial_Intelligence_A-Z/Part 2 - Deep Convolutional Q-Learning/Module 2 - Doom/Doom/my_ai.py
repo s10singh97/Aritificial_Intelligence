87796f3908896809d7c5a5efe3c679c2a36c6db0
s10singh97 <s10singh97@yahoo.in>
2018-12-28 10:36:34 +0530

    Implemented ELigibility Trace

--- Artificial_Intelligence_A-Z/Part 2 - Deep Convolutional Q-Learning/Module 2 - Doom/Doom/my_ai.py
+++ Artificial_Intelligence_A-Z/Part 2 - Deep Convolutional Q-Learning/Module 2 - Doom/Doom/my_ai.py
@@ -74,3 +74,20 @@ ai = AI(brain=cnn, body=softmax_body)
 n_steps = experience_replay.NStepProgress(env=doom_env, ai=ai, n_step=10)
 memory = experience_replay.ReplayMemory(n_steps=n_steps, capacity=10000)
 
+# Implementing Eligibility Trace
+def eligibility_trace(batch):
+    gamma = 0.99
+    inputs = []
+    targets = []
+    for series in batch:
+        input = Variable(torch.from_numpy(np.array([series[0].state, series[-1].state], dtype=float32)))
+        output = cnn(input)
+        cumul_reward = 0.0 if series[-1].done else output[1].data.max()
+        for step in reversed(series[:-1]):
+            cumul_reward = step.reward + gamma * cumul_reward
+        state = series[0].state
+        target = output[0].data
+        target[series[0].action] = cumul_reward
+        inputs.append(state)
+        targets.append(target)
+    return torch.from_numpy(np.array(inputs, dtype=np.float32)), torch.stack(targets)
